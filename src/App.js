f1ea73ae568de9e39914a9ea76d5365a2324e488
Əfraim Məmmədov <frondenddev01@gmail.com>
2024-08-09 13:13:26 +0400

    Remove deleted files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count,setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="container">
+    
+     <div className='main-box'>
+     
+     <div className='box-1'>
+      <button className="minus"onClick={()=>setCount(count-1)}>-</button>
+      <span>{count}</span>
+      <button className="pilus" onClick={()=>setCount(count+1)}>+</button>
+     </div>
+
+    <div className='box-2'>
+    <button className='red' onClick={()=>setCount(Math.sqrt(count))}>√</button>
+    <button className='red' onClick={()=>setCount(count/2)}>/2</button>
+    <button className='red' onClick={()=>setCount(count-2)}>-2</button>
+    
+      <button className='reset' onClick={()=>setCount(count == null)}>reset</button>
+
+      <button className='green' onClick={()=>setCount(count+2)}>+2</button>
+      <button className='green' onClick={()=>setCount(count*2)}>x2</button>
+      <button className='green' onClick={()=>setCount(count*count)}>x²</button>
+    </div>
+     </div>
+    </div>
+    
     </div>
   );
 }
